60e1d977f46be12749d2e3cb8b49dade8641d1d9
kivoimusa <musakivoiz42@gmail.com>
2023-03-20 03:54:04 +0300

    final checks

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,41 +1,33 @@
 #include <stdio.h>
-/**
-*main-Entry point
-*
-*Return: Always 0 (Success/correct)
-*/
 
+/**
+* main - prints all possible combinations of two two-digit numbers
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 int i, j;
-int a, b, c, d;
 
 for (i = 0; i < 100; i++)
 {
-a = i / 10;
-b = i % 10;
-
 for (j = 0; j < 100; j++)
 {
-c = j / 10;
-d = j % 10;
-
-if (a < c || (a == c && b < d))
+if (i < j)
 {
-putchar(a + '0');
-putchar(b + '0');
-putchar(c + '0');
-putchar(d + '0');
-
-if (!(a == 9 && b == 8))
-{
-putchar(44);
-putchar(32);
-}
-}
-}
-}
+putchar((i / 10) + 48);
+putchar((i % 10) + 48);
 putchar(' ');
-putchar(10);
+putchar((j / 10) + 48);
+putchar((j % 10) + 48);
+if (i != 98 || j != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
 return (0);
 }
+
--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -11,7 +11,7 @@ int main(void)
 int x = 0;
 while (x < 10)
 {
-printf("%d, x);
+printf("%d", x);
 x++;
 }
 putchar('\n');
